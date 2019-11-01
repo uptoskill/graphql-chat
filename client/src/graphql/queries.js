1e7968dca67660cc808f14f5d9c0798f65630dd4
Mirko Nasato <mirko@uptoskill.com>
2019-11-01 12:03:09 +0000

    Subscribe to onMessageAdded and display new messages in the frontend

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -21,6 +21,16 @@ const addMessageMutation = gql`
   }
 `;
 
+const messageAddedSubscription = gql`
+  subscription {
+    messageAdded {
+      id
+      from
+      text
+    }
+  }
+`;
+
 export async function addMessage(text) {
   const {data} = await client.mutate({
     mutation: addMessageMutation,
@@ -32,4 +42,9 @@ export async function addMessage(text) {
 export async function getMessages() {
   const {data} = await client.query({query: messagesQuery});
   return data.messages;
+}
+
+export function onMessageAdded(handleMessage) {
+  const observable = client.subscribe({query: messageAddedSubscription});
+  return observable.subscribe(({data}) => handleMessage(data.messageAdded));
 }
