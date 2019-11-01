aa8740a618864a9f445fa39015f12207a18137ae
Mirko Nasato <mirko@nasato.dev>
2019-11-01 11:39:43 +0000

    Enable WebSockets in Apollo Server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const http = require('http');
 const { ApolloServer } = require('apollo-server-express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -40,4 +41,6 @@ app.post('/login', (req, res) => {
   res.send({token});
 });
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+const httpServer = http.createServer(app);
+apolloServer.installSubscriptionHandlers(httpServer);
+httpServer.listen(port, () => console.log(`Server started on port ${port}`));
