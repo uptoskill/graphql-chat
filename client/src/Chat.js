03e327b3a9bab868f4ebf2560836fa5ea9fe2a26
Mirko Nasato <mirko@nasato.dev>
2019-12-07 11:12:08 +0000

    Convert Chat to function component with useState hook

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,43 +1,24 @@
-import React, { Component } from 'react';
-import { addMessage, getMessages, onMessageAdded } from './graphql/queries';
+import React, { useState } from 'react';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
 
-class Chat extends Component {
-  state = {messages: []};
-  subscription = null;
+const Chat = ({user}) => {
+  const [messages, setMessages] = useState([]);
 
-  async componentDidMount() {
-    const messages = await getMessages();
-    this.setState({messages});
-    this.subscription = onMessageAdded((message) => {
-      this.setState({messages: this.state.messages.concat(message)});
-    });
-  }
+  const handleSend = (text) => {
+    const message = {id: text, from: 'you', text};
+    setMessages(messages.concat(message));
+  };
 
-  componentWillUnmount() {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-  }
-
-  async handleSend(text) {
-    await addMessage(text);
-  }
-
-  render() {
-    const {user} = this.props;
-    const {messages} = this.state;
-    return (
-      <section className="section">
-        <div className="container">
-          <h1 className="title">Chatting as {user}</h1>
-          <MessageList user={user} messages={messages} />
-          <MessageInput onSend={this.handleSend.bind(this)} />
-        </div>
-      </section>
-    );
-  }  
-}
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Chatting as {user}</h1>
+        <MessageList user={user} messages={messages} />
+        <MessageInput onSend={handleSend} />
+      </div>
+    </section>
+  );
+};
 
 export default Chat;
