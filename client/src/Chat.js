6dd83e0c93de0e5eb3ba3b0dfa3d5095e735cd72
Mirko Nasato <mirko@uptoskill.com>
2019-12-07 11:23:08 +0000

    Receive new messages with useSubscription

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,13 +1,20 @@
-import { useQuery, useMutation } from '@apollo/react-hooks';
-import React from 'react';
-import { addMessageMutation, messagesQuery } from './graphql/queries';
+import { useQuery, useMutation, useSubscription } from '@apollo/react-hooks';
+import React, { useState } from 'react';
+import { addMessageMutation, messageAddedSubscription, messagesQuery } from './graphql/queries';
 import MessageInput from './MessageInput';
 import MessageList from './MessageList';
 
 const Chat = ({user}) => {
-  const {data} = useQuery(messagesQuery);
+  const [messages, setMessages] = useState([]);
+  useQuery(messagesQuery, {
+    onCompleted: ({messages}) => setMessages(messages)
+  });
+  useSubscription(messageAddedSubscription, {
+    onSubscriptionData: ({subscriptionData}) => {
+      setMessages(messages.concat(subscriptionData.data.messageAdded));
+    }
+  });
   const [addMessage] = useMutation(addMessageMutation);
-  const messages = data ? data.messages : [];
 
   const handleSend = async (text) => {
     await addMessage({variables: {input: {text}}});
